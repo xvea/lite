8a48341bf06ca5b507140f37d79f561ca2c03277
Suryasan <111216828+arnlea@users.noreply.github.com>
2023-02-27 14:58:50 +0700

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -2,8 +2,6 @@ import Navbar from './Navbar';
 import Title from './Title';
 import LinkInput from './LinkInput';
 import LinkOutput from './LinkOutput';
-import Wave from './Wave';
 import Error from './Error';
-import Spinner from './Spinner';
 
-export { Navbar, Title, LinkInput, LinkOutput, Wave, Error, Spinner };
+export { Navbar, Title, LinkInput, LinkOutput, Error };
