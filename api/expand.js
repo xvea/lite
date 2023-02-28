8f3c8d6c1b9dd378cd5c9f76eda6ce25d05fb71d
Suryasan <111216828+arnlea@users.noreply.github.com>
2023-02-28 09:01:12 +0700

    Update expand.js

--- api/expand.js
+++ api/expand.js
@@ -1,39 +1,62 @@
 const admin = require('firebase-admin');
 
 // initialize firebase admin
+
 if (admin.apps.length === 0) {
+
   // load environment variables
+
   require('dotenv').config();
+
   admin.initializeApp({
+
     credential: admin.credential.cert(JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT)),
+
   });
+
 }
 
 // db reference
+
 const db = admin.firestore();
 
 exports.handler = async (req, ctx, res) => {
+
   // get path
+
   const { path } = req;
 
   // get code
+
   const code = path.replace('/', '');
 
   // look up code in database
+
   const links = await db.collection('links').where('code', '==', code).get();
 
   // check if we found one
+
   if (!links.empty) {
+
     // get link
+
     let link = links.docs[0].data().link;
 
     // if link doesnt have protocol, add one (secure by default)
+
     if (!link.startsWith('http')) link = 'https://' + link;
 
     // 301 to link of matching code
-    res.redirect(301, `${link}`)
+
+    res(null, { statusCode: 301, headers: { Location: link } });
+
   } else {
+
     // just 301 to homepage
+
     res(null, { statusCode: 301, headers: { Location: 'http://' + req.headers.host } });
+
   }
+
 };
+
